4fae447b3c02aa39dfe8e862bcd5862fd9c14ca4
nekodusha <BOOTHAlessandra@tutanota.com>
2024-07-25 09:52:41 +0100

    clear all rust files and restart anew

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,14 @@
-#![feature(path_file_prefix)]
-#![feature(exact_size_is_empty)]
-// use std::ffi::OsStr;
+mod setup;
 
-mod api;
-mod endpoints;
-mod init;
-
-use api::server_loop;
-use init::{build, get_meta};
+use setup::init;
 
 fn main() {
-    // this is mut because user can provide their own root dir
-    // to server in the program args at startup
-    let mut server_meta = get_meta();
-    build(&mut server_meta);
+    // start the server
+    // let conn = init();
 
-    println!(
-        "serving directory: {} on url:port: {}",
-        server_meta.dir(),
-        server_meta.ip_addr()
-    );
+    // eprintln!("{:?}", conn);
 
-    let addr = server_meta.listener_addr();
-    let listener = std::net::TcpListener::bind(addr).unwrap();
-
-    server_loop(listener, server_meta.dir());
+    // for stream in conn.incoming() {
+    //     eprintln!("\r\n{:?}", stream);
+    // }
 }
